ce9ad521b60d36d8c1d7cab5c2679fd1ed9e33f8
AJ-Tan <aldomin22@gmail.com>
2026-03-12 17:06:22 +0800

    Improved home ui.

--- src/Components/MainContent/Home/Home.tsx
+++ src/Components/MainContent/Home/Home.tsx
@@ -17,29 +17,31 @@ function Home() {
   return (
     <div className="home">
       <div className="home-wrapper">
-        <section className="home-image">
-          <div className="home-image-wrapper">
-            <img src={storeItem.image} alt="" />
-          </div>
-        </section>
-        <section className="home-details">
-          <header className="home-header">
-            <span className="home-header-eyebrow">BEST SELLING</span>
-            <h2 className="home-header-text">{storeItem.title}</h2>
-          </header>
-          <p className="home-details-description">
-            {storeItem.description[0].toUpperCase() +
-              storeItem.description.slice(1)}
-          </p>
-          <div className="home-details-info">
-            <div className="details-price">${storeItem.price}</div>
-            <Rating rating={storeItem.rating} />
-          </div>
+        <div className="home-content">
+          <section className="home-image">
+            <div className="home-image-wrapper">
+              <img src={storeItem.image} alt="" />
+            </div>
+          </section>
+          <section className="home-details">
+            <header className="home-header">
+              <span className="home-header-eyebrow">BEST SELLING</span>
+              <h2 className="home-header-text">{storeItem.title}</h2>
+            </header>
+            <p className="home-details-description">
+              {storeItem.description[0].toUpperCase() +
+                storeItem.description.slice(1)}
+            </p>
+            <div className="home-details-info">
+              <div className="details-price">${storeItem.price}</div>
+              <Rating rating={storeItem.rating} />
+            </div>
 
-          <Link className="primary-button" to="products">
-            Shop now!
-          </Link>
-        </section>
+            <Link className="primary-button" to="products">
+              Shop now!
+            </Link>
+          </section>
+        </div>
       </div>
     </div>
   );
