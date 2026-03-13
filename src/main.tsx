7af72be9a8591c7b663eaa4e415eebe1fc0444fd
AJ-Tan <aldomin22@gmail.com>
2026-03-13 13:25:59 +0800

    Updated from browser router to hash router for deployment.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createHashRouter, RouterProvider } from "react-router";
 import { routes } from "./routes.tsx";
 
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
